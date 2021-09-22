e3fc7253db3a00bdb9c26836b8e21ee15fb81e9c
otaro36 <68829944+otaro36@users.noreply.github.com>
2021-09-22 15:20:38 -0500

    color basemap

--- SuperTrackX1.0/Assets/Modelos/ScritableObjet/Mini.cs
+++ SuperTrackX1.0/Assets/Modelos/ScritableObjet/Mini.cs
@@ -10,11 +10,42 @@ public class Mini : MonoBehaviour
     void Start()
     {
         render = GetComponent<MeshRenderer>();
+        //render.material.shader = Shader.Find("HDRP/Li");
         z = config_mini.z;
     }
     void Update()
     {
-        render.material.SetTexture("_BaseColorMap", config_mini.texture[config_mini.z]);
+        //render.material.SetTexture("_BaseColorMap", config_mini.texture[config_mini.z]);
+        //render.material.SetColor("_Color", color.gamma);
+        if (z == 0)
+        {
+            render.material.SetColor("_BaseColor", Color.black);
+        }
+        else if (z == 1)
+        {
+            render.material.SetColor("_BaseColor", Color.blue);
+        }
+        else if (z == 2)
+        {
+            render.material.SetColor("_BaseColor", Color.red);
+        }
+        else if (z==3)
+        {
+            render.material.SetColor("_BaseColor", Color.yellow);
+        }
+        else if (z==4)
+        {
+            render.material.SetColor("_BaseColor", Color.green);
+        }
+        else if (z==5)
+        {
+            render.material.SetColor("_BaseColor", Color.grey);
+        }
+        else if (z==6)
+        {
+            render.material.SetColor("_BaseColor", Color.white);
+        }
+        //render.material.SetColor("_BaseColor", Color.blue);
         config_mini.z = z;
     }
     public void CambioCol(int color)
